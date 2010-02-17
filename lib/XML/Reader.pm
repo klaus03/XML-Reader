f0d4c4861031bd7e539af9f30d7ae9c6c3e25777
Klaus Eichner <klaus03@gmail.com>
2010-02-17 17:32:56 +0100

    XML-Reader-0.32.tar.gz -- Git Repo XML::Reader, ver 0.32
    [BUG FIX]
    - Fix a problem in Build.PL which had an incorrect version use 5.010; the correct
    version is use 5.008;

--- lib/XML/Reader.pm
+++ lib/XML/Reader.pm
@@ -12,7 +12,7 @@ our @ISA         = qw(Exporter);
 our %EXPORT_TAGS = ( all => [ qw(slurp_xml) ] );
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT      = qw();
-our $VERSION     = '0.31';
+our $VERSION     = '0.32';
 
 sub newhd {
     my $class = shift;
@@ -581,7 +581,7 @@ sub slurp_xml {
 
 package XML::Reader::Token;
 
-our $VERSION = '0.31';
+our $VERSION = '0.32';
 
 sub found_start_tag   { $_[0][0] eq '<'; }
 sub found_end_tag     { $_[0][0] eq '>'; }
