f62b1c0e77c5c3a325da563eb67a656e926b88a1
Klaus Eichner <klaus03@gmail.com>
2014-08-24 18:23:41 +0100

    XML-Reader-0.64.tar.gz -- Git Repo XML::Reader, ver 0.64
    [DOCUMENTATION]
    - Add a .gitignore file (suggested by Gabor SZABO)
    - Specify the actual version for the depenency Acme::HTTP in Makefile.PL

--- lib/XML/Reader.pm
+++ lib/XML/Reader.pm
@@ -15,7 +15,7 @@ our @ISA         = qw(Exporter);
 our %EXPORT_TAGS = ( all => [ qw(slurp_xml) ] );
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT      = qw();
-our $VERSION     = '0.63';
+our $VERSION     = '0.64';
 
 my $use_module;
 
@@ -1000,7 +1000,7 @@ sub slurp_xml {
 
 package XML::Reader::Token;
 
-our $VERSION = '0.63';
+our $VERSION = '0.64';
 
 sub found_start_tag   { $_[0][0] eq '<'; }
 sub found_end_tag     { $_[0][0] eq '>'; }
